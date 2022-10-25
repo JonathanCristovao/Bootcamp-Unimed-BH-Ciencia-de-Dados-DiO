8d17810c6555c48f636bc78464a6fe808fdaec04
Jonathan Cristovão <74277960+JonathanCristovao@users.noreply.github.com>
2022-10-25 12:57:38 -0300

    Add files via upload
    add files

---
+++ python-para-cientista-de-dados/package_image_processing/package_image_processing_DiO/processing/combination.py
@@ -0,0 +1,19 @@
+import numpy as np
+from skimage.exposure import match_histograms
+from skimage.metrics import structural_similarity
+
+
+def find_difference(image1, image2):
+  
+    assert image1.shape == image2.shape, "Specify 2 images with de same shape."
+    gray_image1 = rgb2gray(image1)
+    gray_image2 = rgb2gray(image2)
+    (score, difference_image) = structural_similarity(gray_image1, gray_image2, full=True)
+    print("Similarity of the images:", score)
+    normalized_difference_image = (difference_image-np.min(difference_image))/(np.max(difference_image)-np.min(difference_image))
+    return normalized_difference_image
+
+def transfer_histrogram(image1, image2):
+
+    matched_image = match_histograms(image1, image2, multichannel=True)
+    return matched_image
